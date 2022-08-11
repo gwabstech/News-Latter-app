bb0833712568e16f61c2d87377d3898443523104
Abubakar Abdullahi Gwabare <gwabstech@gmail.com>
2022-08-12 00:24:43 +0100

    progressing

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const request = require("request")
 const app = express()
 const port = 3000
 
+app.use(express.static("public"))
 app.listen(port,()=>{
     console.log("Server is runing on port 3000")
+})
+
+app.get("/" ,(req,res)=>{
+    res.sendFile(__dirname+"/signup.html")
 })
