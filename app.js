bedf9f0f6c2d748bbf28fe4527ce06a9ceb2032d
Abubakar Abdullahi Gwabare <gwabstech@gmail.com>
2022-08-16 23:55:53 +0100

    done with UI

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express()
 const port = 3000
 
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}))
+
 app.listen(port,()=>{
     console.log("Server is runing on port 3000")
 })
@@ -13,3 +15,14 @@ app.listen(port,()=>{
 app.get("/" ,(req,res)=>{
     res.sendFile(__dirname+"/signup.html")
 })
+
+app.post("/",(req,res)=>{
+    var fName = req.body.fName
+    var lName = req.body.lName
+    var email = req.body.email
+
+    console.log(fName,lName,email)
+})
+
+// api key
+//38b3fc234fbe55ac3d27b465686b16c0-us13
